ea4382ae4cb493771dfb316a8270afc201ec59a8
Biswa Nath Ghosh <tapos.aa@gmail.com>
2018-04-08 13:17:56 +0600

    intendation updated

--- tests/Message/PurchaseRequestTest.php
+++ tests/Message/PurchaseRequestTest.php
@@ -10,15 +10,15 @@ class PurchaseRequestTest extends TestCase
     {
         $request = new PurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
 
-        $request->setMerchantId('128700');
-        $request->setMerchantKey('RZ:Logvisc');
+        $request->setMerchantId('990040129');
+        $request->setMerchantKey('73ICB:M1Z9');
         $request->setQuantity('1');
         $request->setExpectedSettlementDate('03-27-2018');
         $request->setAmount('100.00');
         $request->setReturnUrl('http://example.com/return');
         $requestData = $request->getData();
-        $this->assertEquals($requestData['MerchantId'], '128700');
-        $this->assertEquals($requestData['MerchantKey'], 'RZ:Logvisc');
+        $this->assertEquals($requestData['MerchantId'], '990040129');
+        $this->assertEquals($requestData['MerchantKey'], '73ICB:M1Z9');
         $this->assertEquals($requestData['NoOfItems'], '1');
         $this->assertEquals($requestData['OrderId'], '2');
         $this->assertEquals($requestData['OrderAmount'], '100.00');
